25c8e71e9f8e7a0cd82d59a52bf13568d1673fad
Malek Omri <omrimalektn@gmail.com>
2023-02-17 13:24:53 -0600

    Edited the DAOUtility method to get results as Json

--- src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/coursedb/dao/DAOUtility.java
@@ -8,7 +8,6 @@ package edu.jsu.mcis.cs310.coursedb.dao;
 
 import java.sql.*;
 import com.github.cliftonlabs.json_simple.*;
-import java.util.ArrayList;
 
 public class DAOUtility {
     
@@ -22,9 +21,20 @@ public class DAOUtility {
         
             if (rs != null) {
 
-                // INSERT YOUR CODE HERE
-
-            }
+               
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+                    while (rs.next()) {
+                        JsonObject record = new JsonObject();
+                        for (int i = 1; i <= columnCount; i++) {
+                            String columnName = metaData.getColumnName(i);
+                            Object columnValue = rs.getObject(i);
+                            record.put(columnName, columnValue);
+                        }
+                        records.add(record);
+                    }
+                }
+            
             
         }
         catch (Exception e) {
